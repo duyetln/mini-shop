f636782295f1bba2a44cf9ec0c03dd4dfd53ed84
Duyet Nguyen <duyetlnguyen@gmail.com>
2013-11-28 17:01:50 -0800

    Add customer model

---
+++ accounts-svc/lib/models/customer.rb
@@ -0,0 +1,38 @@
+class Customer < ActiveRecord::Base
+
+  validates :first_name,  presence: true, format: { with: /\A[a-zA-Z]+\z/ }
+  validates :last_name,   presence: true, format: { with: /\A[a-zA-Z]+\z/ }
+  validates :email,       presence: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }, uniqueness: true
+  validates :birthdate,   presence: true
+  validates :password,    presence: true
+
+  before_create :set_uuid
+  before_create :set_confirmation_code
+  before_save   :encrypt_password
+
+  def self.authenticate(uuid, password)
+    customer = self.find_by_uuid(uuid)
+    customer.present? && customer.confirmed? && BCrypt::Password.new(customer.password) == password ? customer : nil
+  end
+
+  def set_uuid
+    self.uuid = SecureRandom.hex(10)
+  end
+
+  def set_confirmation_code
+    self.confirmation_code = SecureRandom.hex
+  end
+
+  def encrypt_password
+    self.password = BCrypt::Password.create(self.password) if self.password_changed?
+  end
+
+  def confirmed?
+    self.confirmation_code.blank?
+  end
+
+  def confirm!
+    !confirmed? && self.update_attributes(confirmation_code: nil)
+  end
+
+end
