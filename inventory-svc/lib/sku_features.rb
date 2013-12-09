f6b45d7a27eb1c4ea13ea72ed0cc4e0add5256de
Duyet Nguyen <duyetlnguyen@gmail.com>
2013-12-09 01:00:48 -0800

    Extract common sku behaviours to a module

---
+++ inventory-svc/lib/sku_features.rb
@@ -0,0 +1,33 @@
+module SkuFeatures
+  extend ActiveSupport::Concern
+
+  included do
+    validates :title, presence: true
+    scope :active,   where(active: true)
+    scope :inactive, where(active: false)
+
+    before_create :set_active 
+  end
+
+  def set_active
+    self.active = true if self.active.nil?
+  end
+
+  def available?
+    self.active?
+  end
+
+  def activate!
+    self.active = true
+    self.save
+  end
+
+  def deactivate!
+    self.active = false
+    self.save
+  end
+
+  def fulfill!(order)
+  end
+
+end
