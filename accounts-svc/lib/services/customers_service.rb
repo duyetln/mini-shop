605c6c5bb984e1df9385a3e4b94255dd404a0e97
Duyet Nguyen <duyetlnguyen@gmail.com>
2013-12-14 22:16:37 -0800

    Update customer service, use id as search key

--- accounts-svc/lib/services/customers_service.rb
+++ accounts-svc/lib/services/customers_service.rb
@@ -2,43 +2,65 @@ class CustomersService < Sinatra::Base
 
   before { content_type :json }
 
-  get "/customers/:uuid" do
-    customer = load_customer
-    customer.present? ? customer.to_json(customer_response_options) : error(404)
+  helpers do
+
+    protected 
+
+    def find_customer_by_id!
+      @customer = Customer.find_by_id(params[:id]) || halt(404)
+    end
+
+    def find_customer_by_uuid!
+      @customer = Customer.find_by_uuid(params[:uuid]) || halt(404)
+    end
+
+    def customer_response_options
+      { except: [:password, :updated_at] }
+    end
+
+    def load_customer!
+      @customer = Customer.new(accessible_params)
+      @customer.valid? && @customer || halt(400)
+    end
+
+    def accessible_params
+      params.slice *Customer.accessible_attributes.to_a
+    end
+
+    def respond_with(resource, response_options={}, json_options=customer_response_options)
+      resource.present? || halt(404)
+      status = block_given? ? yield(resource) : resource
+      success_code = response_options[:success] || 200
+      failure_code = response_options[:failure] || 500
+      status ? halt(success_code, resource.to_json(json_options)) : halt(failure_code)
+    end
+
+  end
+
+  get "/customers/:id" do
+    find_customer_by_id!
+    respond_with(@customer)
   end
 
   post "/customers" do
-    customer = Customer.new(accessible_params)
-    customer.save ? customer.to_json(customer_response_options) : error(500)
+    load_customer!
+    respond_with(@customer) { |c| c.save }
   end
 
   post "/customers/authenticate" do
-    customer = Customer.authenticate(params[:uuid], params[:password])
-    customer.present? ? customer.to_json(customer_response_options) : error(404)
+    @customer = Customer.authenticate(params[:uuid], params[:password])
+    respond_with(@customer)
   end
 
-  put "/customers/:uuid" do
-    customer = load_customer
-    customer.present? ? ( customer.update_attributes(accessible_params) ? customer.to_json(customer_response_options) : error(500) ) : error(404)
+  put "/customers/:id" do
+    find_customer_by_id!
+    respond_with(@customer, failure: 400) { |c| c.update_attributes(accessible_params) }
   end
 
   put "/customers/:uuid/confirm/:confirmation_code" do
-    customer = load_customer
-    customer.present? && !customer.confirmed? && customer.confirmation_code == params[:confirmation_code] ? ( customer.confirm! ? customer.to_json(customer_response_options) : error(500) ) : error(404)
-  end
-
-  protected 
-
-  def customer_response_options
-    { except: [:id, :password, :updated_at]  }
-  end 
-
-  def load_customer
-    Customer.find_by_uuid(params[:uuid])
-  end
-
-  def accessible_params
-    params.slice *Customer.accessible_attributes.to_a
+    find_customer_by_uuid!
+    !@customer.confirmed? && @customer.confirmation_code == params[:confirmation_code] || halt(404)
+    respond_with(@customer) { |c| c.confirm! }
   end
 
 end
