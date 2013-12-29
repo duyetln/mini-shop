12eb085292405f6fe1c18be94cb04b3cefc2fa7a
Duyet Nguyen <duyetlnguyen@gmail.com>
2013-12-29 13:48:46 -0800

    Add physical sku spec

---
+++ inventory-svc/spec/models/physical_sku_spec.rb
@@ -0,0 +1,53 @@
+require "spec_helper"
+require "sku_resource_spec"
+
+describe PhysicalSku do
+
+  include SkuResourceSpec
+
+  context "accessible attributes" do
+
+    before(:each) { @attributes = described_class.accessible_attributes.to_a.map(&:to_sym) }
+    
+    it "includes quantity" do
+
+       expect(@attributes).to include(:quantity)
+    end
+  end
+
+  context "#quantity" do
+
+    context "emtpy" do
+
+      it "is not valid" do
+
+        @factory_sku.quantity = nil
+        expect(@factory_sku.valid?).to be_false
+        expect(@factory_sku.errors).to have_key(:quantity)
+      end
+    end
+
+    context "negative value" do
+
+      it "is not valid" do
+
+        @factory_sku.quantity = -5
+        expect(@factory_sku.valid?).to be_false
+        expect(@factory_sku.errors).to have_key(:quantity)
+      end
+    end
+  end
+
+  context "#available?" do
+
+    context "negative quantity" do
+
+      it "is false" do
+
+        @factory_sku.quantity = -5
+        expect(@factory_sku.available?).to be_false
+      end
+    end
+  end
+
+end
