dcd0f1ad10228e10d9f810c77b9e5b74656c5abb
Duyet Nguyen <duyetlnguyen@gmail.com>
2013-12-29 12:15:33 -0800

    Fix typos

--- inventory-svc/lib/models/bundling.rb
+++ inventory-svc/lib/models/bundling.rb
@@ -5,9 +5,9 @@ class Bundling < ActiveRecord::Base
   belongs_to :bundle_sku
   belongs_to :bundled_sku, polymorphic: true
 
-  validates :bundle_sku,  presence: true
-  validates :bundled_sku, presence: true
-  validates :bundle_sku_id,   uniqueness: { scope: [ :bundled_sku_id, :bundled_sku_type ] }
-  validates :bundle_sku_type, inclusion:  { in: BUNDLED_SKU_TYPES.map(&:to_s) }
+  validates :bundle_sku,    presence: true
+  validates :bundle_sku_id, uniqueness: { scope: [ :bundled_sku_id, :bundled_sku_type ] }
+  validates :bundled_sku,      presence: true
+  validates :bundled_sku_type, inclusion:  { in: BUNDLED_SKU_TYPES.map(&:to_s) }
 
 end
