4c0f51c9aa155fcb00816b63cc5d25b276580db1
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-01-12 20:17:03 -0800

    Create pricepoint_prices table

---
+++ inventory-svc/db/migrate/20140113032319_create_pricepoint_prices_table.rb
@@ -0,0 +1,18 @@
+class CreatePricepointPricesTable < ActiveRecord::Migration
+  def up
+    create_table :pricepoint_prices do |t|
+      t.decimal  :amount, precision: 8, scale: 4
+      t.integer  :pricepoint_id
+      t.integer  :currency_id
+    end
+
+    add_index    :pricepoint_prices, :pricepoint_id
+    add_index    :pricepoint_prices, :currency_id
+  end
+
+  def down
+    remove_index :pricepoint_prices, :currency_id
+    remove_index :pricepoint_prices, :pricepoint_id
+    drop_table   :pricepoint_prices
+  end
+end
