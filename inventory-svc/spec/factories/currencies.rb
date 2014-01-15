a436aed3f322936eded4146d149d0c2f3c523388
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-01-14 23:28:55 -0800

    Add currencies factory

---
+++ inventory-svc/spec/factories/currencies.rb
@@ -0,0 +1,13 @@
+FactoryGirl.define do
+  factory :usd, class: Currency do
+    code "USD"
+  end
+
+  factory :eur, class: Currency do
+    code "EUR"
+  end
+
+  factory :gbp, class: Currency do
+    code "GBP"
+  end
+end
