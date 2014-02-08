fe0d78d1d6d57700ac604ece40ca6f4ccf84c64e
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-02-08 00:49:08 -0800

    Update user service spec

--- backend/spec/services/users_service_spec.rb
+++ backend/spec/services/users_service_spec.rb
@@ -2,153 +2,135 @@ require "spec_helper"
 
 describe UsersService do
 
-  def accessible_attributes(record)
-    record.attributes.slice *record.class.accessible_attributes.to_a
-  end
-
   def user_response_options
     { except: [:password, :updated_at]  }
   end
 
   describe "get /users/:id" do
 
-    before(:all) { @user = FactoryGirl.create :user }
+    context "valid id" do
 
-    context "user found" do
+      it "returns the user" do
 
-      it "should return the user" do
-        get "/users/#{@user.id}"
-        expect(last_response.status).to eq(200)
-        expect(last_response.body).to   eq(@user.to_json(user_response_options))
+        get "/users/#{created_user.id}"
+        expect_status(200)
+        expect(parsed_result[:id]).to eq(created_user.id)
       end
     end
 
-    context "user not found" do
+    context "invalid id" do
 
-      it "should return 404 status" do
+      it "returns 404 status" do
+
         get "/users/#{random_string}"
-        expect(last_response.status).to eq(404)
+        expect_status(404)
+        expect_empty_response
       end
     end
   end
 
   describe "post /users" do
-
-    before(:each) { @user = FactoryGirl.build :user }
     
-    context "user created" do
+    context "valid parameters" do
 
-      it "should return the user" do
-        post "/users", accessible_attributes(@user)
-        expect(last_response.status).to eq(200)
-        expect(last_response.body).to be_present
-        uuid = Yajl::Parser.parse(last_response.body, symbolize_keys: true)[:user][:uuid]
-        expect(User.find_by_uuid(uuid)).to be_present
+      it "creates the user and returns it" do
+
+        expect{ post "/users", built_user.attributes }.to change{ User.count }.by(1)
+        expect_status(200)
+        expect(parsed_result[:id]).to eq(User.last.id)
+        expect(parsed_result[:uuid]).to eq(User.last.uuid)
       end
     end
 
-    context "user not created" do
 
-      context "invalid input" do
+    context "missing parameters" do
 
-        it "should return 400 status" do
+      it "returns 400 status" do
 
-          previous_count = User.count
-          user_hash  = accessible_attributes(@user)
-
-          post "/users", user_hash.except(user_hash.keys.sample)
-          expect(last_response.status).to eq(400)
-          expect(User.count).to eq(previous_count)
-        end
+        expect{ post "/users" }.to_not change{ User.count }
+        expect_status(400)
+        expect_empty_response
       end
+    end
 
-      context "valid input" do
+    context "user creation failed" do
 
-        it "should return 500 status" do
+      it "returns 500 status" do
 
-          previous_count = User.count
-          user_hash  = accessible_attributes(@user)
-          expect(User).to receive(:new).with(user_hash.as_json).and_return(@user)
-          expect(@user).to receive(:valid?).and_return(true)
-          expect(@user).to receive(:save).and_return(false)
+        expect(User).to receive(:new).with(an_instance_of(Hash)).and_return(built_user)
+        expect(built_user).to receive(:valid?).and_return(true)
+        expect(built_user).to receive(:save).and_return(false)
 
-          post "/users", user_hash
-          expect(last_response.status).to eq(500)
-          expect(User.count).to eq(previous_count)
-        end
+        expect{ post "/users", built_user.attributes }.to_not change{ User.count }
+        expect_status(500)
       end
     end
   end
 
   describe "post /users/authenticate" do
 
-    before :each do
-      @user = FactoryGirl.build :user
-      @password = @user.password
-      @user.save
-    end
+    let(:uuid) { created_user.uuid }
+    let(:password) { built_user.password }
 
     context "user authenticated" do
 
-      it "should return the user" do
-        @user.confirm!
-        post "/users/authenticate", { uuid: @user.uuid, password: @password }
-        expect(last_response.status).to eq(200)
-        expect(last_response.body).to be_present
-
-        uuid = Yajl::Parser.parse(last_response.body, symbolize_keys: true)[:user][:uuid]
-        expect(uuid).to eq(@user.uuid)
+      it "returns the user" do
+        
+        created_user.confirm!
+        post "/users/authenticate", { uuid: uuid, password: password }
+        expect_status(200)
+        expect(parsed_result[:uuid]).to eq(uuid)
       end
     end
 
     context "user not authenticated" do
-      context("not confirmed")  { it("should return 404 status") {                     post "/users/authenticate", { uuid: @user.uuid, password: @password };     expect(last_response.status).to eq(404) } }
-      context("wrong uuid")     { it("should return 404 status") { @user.confirm!; post "/users/authenticate", { uuid: random_string,  password: @password };     expect(last_response.status).to eq(404) } }
-      context("wrong password") { it("should return 404 status") { @user.confirm!; post "/users/authenticate", { uuid: @user.uuid, password: random_string }; expect(last_response.status).to eq(404) } }
+
+      context("not confirmed")  { it("returns 404 status") {                        post "/users/authenticate", { uuid: uuid,           password: password };       expect_status(404); expect_empty_response } }
+      context("wrong uuid")     { it("returns 404 status") { created_user.confirm!; post "/users/authenticate", { uuid: random_string,  password: password };       expect_status(404); expect_empty_response } }
+      context("wrong password") { it("returns 404 status") { created_user.confirm!; post "/users/authenticate", { uuid: uuid,           password: random_string };  expect_status(404); expect_empty_response } }
     end
 
   end
 
   describe "put /users/:id" do
 
-    before :each do
-      @user = FactoryGirl.build :user
-      @password = @user.password
-      @user.save
+    context "user not found" do
+
+      it "returns 404 status" do 
+
+        put "/users/#{random_string}"
+        expect_status(404)
+        expect_empty_response
+      end
     end
 
-    context("user not found") { it("should return 404 status") { put "/users/#{random_string}"; expect(last_response.status).to eq(404) } }
     context "user found" do
 
-      context "invalid input" do
+      context "invalid parameters" do
 
-        it "should return 400 status" do
-          user_hash = accessible_attributes(@user)
-          user_hash[user_hash.keys.sample] = nil
-          put "/users/#{@user.id}", user_hash
-          expect(last_response.status).to eq(400)
-          @user.reload
-          expect(accessible_attributes(@user)).to eq(accessible_attributes(User.find_by_uuid(@user.uuid)))
+        it "returns 400 status" do
+
+          expect{ 
+            put "/users/#{created_user.id}", first_name: nil
+            created_user.reload 
+          }.to_not change{ created_user }
+          expect_status(400)
         end
       end
 
-      context "valid input" do
+      context "valid parameters" do
 
-        it "should return the user" do
-          user_hash = accessible_attributes(@user)
-          new_password   = random_string
-          new_first_name = random_string.gsub(/\d/,"")
-          user_hash["password"]   = new_password
-          user_hash["first_name"] = new_first_name
+        let(:new_password) { random_string }
+        let(:new_first_name) { "John" }
 
-          put "/users/#{@user.id}", user_hash.slice("password", "first_name")
-          expect(last_response.status).to eq(200)
-          expect(last_response.body).to be_present
+        it "returns the user" do
 
-          uuid = Yajl::Parser.parse(last_response.body, symbolize_keys: true)[:user][:uuid]
-          updated_user = User.find_by_uuid(uuid)
-          expect(updated_user.first_name).to eq(new_first_name)
-          expect(BCrypt::Password.new updated_user.password).to eq(new_password)
+          put "/users/#{created_user.id}", password: new_password, first_name: new_first_name
+          created_user.reload
+          expect_status(200)
+          expect(parsed_result[:uuid]).to eq(created_user.uuid)
+          expect(created_user.first_name).to eq(new_first_name)
+          expect(BCrypt::Password.new(created_user.password)).to eq(new_password)
         end
       end
     end
@@ -156,32 +138,52 @@ describe UsersService do
 
   describe "put /users/:uuid/confirm/:confirmation_code" do
 
-    before :each do
-      @user = FactoryGirl.create :user
-      @uuid              = @user.uuid
-      @confirmation_code = @user.confirmation_code
+    context "user not found" do
+
+      it "returns 404 status" do
+
+        put "/users/#{random_string}/confirm/#{created_user.confirmation_code}"
+        created_user.reload
+        expect_status(404)
+        expect(created_user).to_not be_confirmed
+      end
     end
 
-    def ensure_confirmation_status(status)
-      @user.reload
-      expect(@user.confirmed?).to eq(status)
-    end
-
-    context("user not found") { it("should return 404 status") { put "/users/#{random_string}/confirm/#{@confirmation_code}"; expect(last_response.status).to eq(404); ensure_confirmation_status(false) } }
     context "user found" do
 
-      context("confirmed user")            { it("should return 404 status") { @user.confirm!; put "/users/#{@uuid}/confirm/#{@confirmation_code}"; expect(last_response.status).to eq(404); ensure_confirmation_status(true) } }
-      context("wrong confirmation code")   { it("should return 404 status") {                     put "/users/#{@uuid}/confirm/#{@random_code}";       expect(last_response.status).to eq(404); ensure_confirmation_status(false)} }
+      context "confirmed user" do 
+
+        it "returns 404 status" do
+        
+          created_user.confirm!
+
+          put "/users/#{created_user.uuid}/confirm/#{created_user.confirmation_code}"
+          created_user.reload
+          expect_status(404)
+          expect(created_user).to be_confirmed
+        end
+      end
+
+      context "wrong confirmation code" do 
+
+        it "returns 404 status" do 
+
+          put "/users/#{created_user.uuid}/confirm/#{random_string}"
+          created_user.reload
+          expect_status(404)
+          expect(created_user).to_not be_confirmed
+        end
+      end
+
       context "correct confirmation code" do 
 
-        it "should return the user" do
-          put "/users/#{@uuid}/confirm/#{@confirmation_code}"
-          expect(last_response.status).to eq(200)
-          expect(last_response.body).to be_present
+        it "returns the user" do
 
-          uuid = Yajl::Parser.parse(last_response.body, symbolize_keys: true)[:user][:uuid]
-          expect(uuid).to eq(@user.uuid)
-          ensure_confirmation_status(true)
+          put "/users/#{created_user.uuid}/confirm/#{created_user.confirmation_code}"
+          created_user.reload
+          expect_status(200)
+          expect(parsed_result[:uuid]).to eq(created_user.uuid)
+          expect(created_user).to be_confirmed
         end
       end
     end
