364f14e1f84c6e41bfd506444d427d2cdf8764a6
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-02-07 22:53:30 -0800

    Add a few common interface modules

---
+++ backend/lib/interface.rb
@@ -0,0 +1,46 @@
+module ActivableScope
+  extend ActiveSupport::Concern
+
+  included do
+
+    scope :active,   -> { where(active: true) }
+    scope :inactive, -> { where(active: false) }
+  end
+end
+
+module Activable
+
+  [:activate!, :deactivate!].each do |method|
+    define_method method do
+      raise "#{method} must be implemented"
+    end
+  end
+end
+
+module RemovableScope
+  extend ActiveSupport::Concern
+
+  included do
+
+    scope :removed,  -> { where(removed: true) }
+    scope :kept,     -> { where(removed: false) }
+  end
+end
+
+module Removable
+
+  [:delete!].each do |method|
+    define_method method do
+      raise "#{method} must be implemented"
+    end
+  end
+end
+
+module Fulfillable
+
+  [:fulfill!, :unfulfill!].each do |method|
+    define_method method do |context|
+      raise "#{method} must be implemented"
+    end
+  end
+end
