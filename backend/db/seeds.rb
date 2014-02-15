90adfc01928132ae21e64655a0e6ff3cdfcce3e1
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-02-14 21:36:04 -0800

    Add GBP currency unit

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -18,6 +18,7 @@ collector_ed = BundleItem.where(title: "StarCraft 2 Collector's Edition").first_
 usd = Currency.where(code: "USD").first_or_create
 eur = Currency.where(code: "EUR").first_or_create
 krw = Currency.where(code: "KRW").first_or_create
+gbp = Currency.where(code: "GBP").first_or_create
 
 no_discount           = Discount.where(name: "No discount", rate: 0.0).first_or_create
 black_friday_discount = Discount.where(name: "Black Friday Discount", rate: 0.75).first_or_create(start_at: DateTime.new(2013,11,25), end_at: DateTime.new(2013,12,9))
@@ -30,14 +31,17 @@ collector_ed_pp = Pricepoint.where(name: "SC2 Collector Pricepoint").first_or_cr
 sc2_standard_pp.pricepoint_prices.where(currency_id: usd.id).first_or_create(amount: 59.99)
 sc2_standard_pp.pricepoint_prices.where(currency_id: eur.id).first_or_create(amount: 44.27)
 sc2_standard_pp.pricepoint_prices.where(currency_id: krw.id).first_or_create(amount: 63907.35)
+sc2_standard_pp.pricepoint_prices.where(currency_id: gbp.id).first_or_create(amount: 35.83)
 
 deluxe_ed_pp.pricepoint_prices.where(currency_id: usd.id).first_or_create(amount: 79.99)
 deluxe_ed_pp.pricepoint_prices.where(currency_id: eur.id).first_or_create(amount: 59.03)
 deluxe_ed_pp.pricepoint_prices.where(currency_id: krw.id).first_or_create(amount: 85213.35)
+deluxe_ed_pp.pricepoint_prices.where(currency_id: gbp.id).first_or_create(amount: 47.78)
 
 collector_ed_pp.pricepoint_prices.where(currency_id: usd.id).first_or_create(amount: 99.99)
 collector_ed_pp.pricepoint_prices.where(currency_id: eur.id).first_or_create(amount: 73.80)
 collector_ed_pp.pricepoint_prices.where(currency_id: krw.id).first_or_create(amount: 106530.00)
+collector_ed_pp.pricepoint_prices.where(currency_id: gbp.id).first_or_create(amount: 59.72)
 
 sc2_standard_price = Price.where(pricepoint_id: sc2_standard_pp.id).first_or_create(name: "SC2 Standard Price", discount_id: no_discount.id)
 deluxe_ed_price    = Price.where(pricepoint_id: deluxe_ed_pp.id).first_or_create(name: "SC2 Deluxe Price", discount_id: no_discount.id)
@@ -47,3 +51,4 @@ StorefrontItem.where(item_type: sc2_retail.class,   item_id: sc2_retail.id,    p
 StorefrontItem.where(item_type: sc2_digital.class,  item_id: sc2_digital.id,   price_id: sc2_standard_price.id).first_or_create(title: "StarCraft 2 Digital Edition")
 StorefrontItem.where(item_type: deluxe_ed.class,    item_id: deluxe_ed.id,     price_id: deluxe_ed_price.id).first_or_create(title: "StarCraft 2 Deluxe Edition")
 StorefrontItem.where(item_type: collector_ed.class, item_id: collector_ed.id,  price_id: collector_ed_price.id).first_or_create(title: "StarCraft 2 Collector's Edition")
+
