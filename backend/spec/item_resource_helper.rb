f793a21cf0660a879f52356006ec2195753bfcff
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-02-22 00:49:10 -0800

    Fix wrong spec description + updates

--- backend/spec/item_resource_helper.rb
+++ backend/spec/item_resource_helper.rb
@@ -34,37 +34,100 @@ shared_examples "item resource" do
     context("inactive") { it("is false") { built_item.active  = false; expect(built_item).to_not be_available } }
   end
 
+  shared_examples "item not allowed to modify active flag" do
+
+    it "does not modify active flag" do
+
+      expect(test_item).to_not receive(:active=)
+      expect(test_item).to_not receive(:save)
+      test_item.send(action)
+    end
+  end
+
+  shared_examples "item not allowed to modify removed flag" do
+
+    it "does not modify removed flag" do
+
+      expect(test_item).to_not receive(:removed=)
+      expect(test_item).to_not receive(:save)
+      test_item.send(action)
+    end
+  end
+
   context "persisted" do
 
     before(:each) { expect(built_item).to receive(:persisted?).and_return(true) }
 
-    describe "#activate!" do
+    let(:test_item) { built_item }
 
-      it "sets active to true and saves" do
+    context "inactive" do
 
-        expect(built_item).to receive(:active=).with(true)
-        expect(built_item).to receive(:save)
-        built_item.activate!
+      before(:each) { expect(built_item).to receive(:active?).and_return(false) }
+
+      describe "#activate!" do
+
+        it "sets active to true and saves" do
+
+          expect(built_item).to receive(:active=).with(true)
+          expect(built_item).to receive(:save)
+          built_item.activate!
+        end
+      end
+
+      describe "#deactivate!" do
+
+        let(:action) { :deactivate! }
+
+        it_behaves_like "item not allowed to modify active flag"
       end
     end
 
-    describe "#deactivate!" do
+    context "active" do
 
-      it "sets active to false and saves" do
+      before(:each) { expect(built_item).to receive(:active?).and_return(true) }
 
-        expect(built_item).to receive(:active=).with(false)
-        expect(built_item).to receive(:save)
-        built_item.deactivate!
+      describe "#activate!" do
+
+        let(:action) { :activate! }
+
+        it_behaves_like "item not allowed to modify active flag"
+      end
+
+      describe "#deactivate!" do
+
+        it "sets active to false and saves" do
+
+          expect(built_item).to receive(:active=).with(false)
+          expect(built_item).to receive(:save)
+          built_item.deactivate!
+        end
       end
     end
 
-    describe "#delete!" do
+    context "kept" do
 
-      it "sets removed to true and saves" do
+      before(:each) { expect(built_item).to receive(:removed?).and_return(false) }
 
-        expect(built_item).to receive(:removed=).with(true)
-        expect(built_item).to receive(:save)
-        built_item.delete!
+      describe "#delete!" do
+
+        it "sets removed to true and saves" do
+
+          expect(built_item).to receive(:removed=).with(true)
+          expect(built_item).to receive(:save)
+          built_item.delete!
+        end
+      end
+    end
+
+    context "removed" do
+
+      before(:each) { expect(built_item).to receive(:removed?).and_return(true) }
+
+      describe "#delete!" do
+
+        let(:action) { :delete! }
+
+        it_behaves_like "item not allowed to modify removed flag"
       end
     end
   end
@@ -73,34 +136,27 @@ shared_examples "item resource" do
 
     before(:each) { expect(built_item).to_not be_persisted }
 
+    let(:test_item) { built_item }
+
     describe "#activate!" do
 
-      it "sets active to true and saves" do
+      let(:action) { :activate! }
 
-        expect(built_item).to_not receive(:active=)
-        expect(built_item).to_not receive(:save)
-        built_item.activate!
-      end
+      it_behaves_like "item not allowed to modify active flag"
     end
 
     describe "#deactivate!" do
 
-      it "sets active to false and saves" do
+      let (:action) { :deactivate! }
 
-        expect(built_item).to_not receive(:active=)
-        expect(built_item).to_not receive(:save)
-        built_item.deactivate!
-      end
+      it_behaves_like "item not allowed to modify active flag"
     end
 
     describe "#delete!" do
 
-      it "sets removed to true and saves" do
+      let(:action) { :delete! }
 
-        expect(built_item).to_not receive(:removed=)
-        expect(built_item).to_not receive(:save)
-        built_item.delete!
-      end
+      it_behaves_like "item not allowed to modify removed flag"
     end
   end
 end
