6b134741142559b4dd75c15afbfa29fb73f9cd07
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-02-24 22:05:13 -0800

    Add ownerships table

---
+++ backend/db/migrate/20140225055814_create_ownerships_table.rb
@@ -0,0 +1,19 @@
+class CreateOwnershipsTable < ActiveRecord::Migration
+  def up
+    create_table :ownerships do |t|
+      t.integer  :user_id
+      t.string   :item_type
+      t.string   :item_id
+      t.datetime :created_at
+    end
+
+    add_index    :ownerships, :user_id
+    add_index    :ownerships, [:item_type, :item_id]
+  end
+
+  def down
+    remove_index :ownerships, [:item_type, :item_id]
+    remove_index :ownerships, :user_id
+    drop_table   :ownerships
+  end
+end
