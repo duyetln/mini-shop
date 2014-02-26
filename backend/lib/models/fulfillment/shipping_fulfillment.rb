ac332e0d5fe832fc09850addf62642bf4df72f15
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-02-25 18:33:16 -0800

    Add ShippingFulfillment child class

---
+++ backend/lib/models/fulfillment/shipping_fulfillment.rb
@@ -0,0 +1,12 @@
+class ShippingFulfillment < Fulfillment
+
+  protected
+
+  def process!
+    shipment = Shipment.new
+    shipment.user = order.user
+    shipment.item = order.item
+    shipment.shipping_address = order.shipping_address
+    shipment.save
+  end
+end
