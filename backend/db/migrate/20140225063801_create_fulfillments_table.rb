f5a56938d84a8da682f7f91caa1b05fce4e3257b
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-02-27 01:40:38 -0800

    Add item association to fulfillment

--- backend/db/migrate/20140225063801_create_fulfillments_table.rb
+++ backend/db/migrate/20140225063801_create_fulfillments_table.rb
@@ -3,6 +3,8 @@ class CreateFulfillmentsTable < ActiveRecord::Migration
     create_table :fulfillments do |t|
       t.string   :type
       t.integer  :order_id
+      t.string   :item_type
+      t.integer  :item_id
       t.boolean  :fulfilled
       t.datetime :fulfilled_at
       t.datetime :created_at
@@ -10,9 +12,11 @@ class CreateFulfillmentsTable < ActiveRecord::Migration
 
     add_index    :fulfillments, :type
     add_index    :fulfillments, :order_id
+    add_index    :fulfillments, [:item_type, :item_id]
   end
 
   def down
+    remove_index :fulfillments, [:item_type, :item_id]
     remove_index :fulfillments, :order_id
     remove_index :fulfillments, :type
     drop_table   :fulfillments
--- backend/lib/models/fulfillment/fulfillment.rb
+++ backend/lib/models/fulfillment/fulfillment.rb
@@ -3,12 +3,20 @@ class Fulfillment < ActiveRecord::Base
   class FulfillmentFailure < StandardError; end
   class UnfulfillmentFailure < StandardError; end
 
-  attr_accessible :order_id
+  attr_accessible :order_id, :item_type, :item_id
+
+  ITEM_TYPES = [ "PhysicalItem", "DigitalItem" ]
 
   belongs_to :order
+  belongs_to :item, polymorphic: true
 
-  validates :order_id, presence: true
-  validates :order,    presence: true
+  validates :order_id,  presence: true
+  validates :item_type, presence: true
+  validates :item_type, inclusion: { in: ITEM_TYPES }
+  validates :item_id,   presence: true
+
+  validates :order, presence: true
+  validates :item,  presence: true
 
   before_create :set_values
 
