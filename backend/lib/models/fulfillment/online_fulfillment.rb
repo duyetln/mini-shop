673a01ac60123b75d63cf99f02a37cbb03636909
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-02-27 01:49:36 -0800

    Update Fulfillment classes

--- backend/lib/models/fulfillment/online_fulfillment.rb
+++ backend/lib/models/fulfillment/online_fulfillment.rb
@@ -3,9 +3,10 @@ class OnlineFulfillment < Fulfillment
   protected
 
   def process!
-    ownership = Ownership.new
+    ownership = Ownership.where(order_id: order.id, item_type: item.class, item_id: item.id).first_or_initialize
     ownership.user = order.user
-    ownership.item = order.item
+    ownership.quantity ||= 0
+    ownership.quantity  += 1
     ownership.save!
   end
 end
--- backend/lib/models/fulfillment/shipping_fulfillment.rb
+++ backend/lib/models/fulfillment/shipping_fulfillment.rb
@@ -3,9 +3,10 @@ class ShippingFulfillment < Fulfillment
   protected
 
   def process!
-    shipment = Shipment.new
+    shipment = Shipment.where(order_id: order.id, item_type: item.class, item_id: item.id).first_or_initialize
     shipment.user = order.user
-    shipment.item = order.item
+    shipment.quantity ||= 0
+    shipment.quantity  += 1
     shipment.shipping_address = order.shipping_address
     shipment.save!
   end
