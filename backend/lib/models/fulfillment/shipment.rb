310fc20f2a4114d35f9732d045778b1d511e8466
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-02-27 01:10:17 -0800

    Use ITEM_TYPES consistently

--- backend/lib/models/fulfillment/shipment.rb
+++ backend/lib/models/fulfillment/shipment.rb
@@ -2,12 +2,15 @@ class Shipment < ActiveRecord::Base
 
   attr_accessible :user_id, :item_type, :item_id, :quantity, :shipping_address_id
 
+  ITEM_TYPES = [ "PhysicalItem" ]
+
   belongs_to :user
   belongs_to :item, polymorphic: true
   belongs_to :shipping_address, class_name: "Address"
 
   validates :user_id,   presence: true
   validates :item_type, presence: true
+  validates :item_type, inclusion: { in: ITEM_TYPES }
   validates :item_id,   presence: true
   validates :quantity,  presence: true
   validates :quantity,  numericality: { greater_than_or_equal_to: 0 }
--- backend/lib/models/inventory/bundling.rb
+++ backend/lib/models/inventory/bundling.rb
@@ -1,6 +1,6 @@
 class Bundling < ActiveRecord::Base
 
-  BUNDLED_ITEM_TYPES = [ "DigitalItem", "PhysicalItem" ]
+  ITEM_TYPES = [ "DigitalItem", "PhysicalItem" ]
 
   belongs_to :bundle_item
   belongs_to :bundled_item, polymorphic: true
@@ -8,6 +8,6 @@ class Bundling < ActiveRecord::Base
   validates :bundle_item,    presence: true
   validates :bundle_item_id, uniqueness: { scope: [ :bundled_item_id, :bundled_item_type ] }
   validates :bundled_item,      presence: true
-  validates :bundled_item_type, inclusion: { in: BUNDLED_ITEM_TYPES }
+  validates :bundled_item_type, inclusion: { in: ITEM_TYPES }
 
 end
