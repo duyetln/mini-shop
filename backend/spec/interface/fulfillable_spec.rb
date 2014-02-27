b87c18b86a140805b0786f1e430796b7f2d0257d
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-02-26 22:09:55 -0800

    Add Fulfillable interface spec

---
+++ backend/spec/interface/fulfillable_spec.rb
@@ -0,0 +1,23 @@
+require "spec_helper"
+
+[PhysicalItem, DigitalItem, BundleItem, StorefrontItem].each do |model|
+
+  describe model do
+
+    let(:instance) { described_class.new }
+
+    it("responds to fulfill!") { expect(instance).to respond_to(:fulfill!).with(1).argument }
+    it("responds to unfulfill!") { expect(instance).to respond_to(:unfulfill!).with(1).argument }
+  end
+end
+
+[Purchase, Order].each do |model|
+
+  describe model do
+
+    let(:instance) { described_class.new }
+
+    it("responds to fulfill!") { expect(instance).to respond_to(:fulfill!).with(0).argument }
+    # it("responds to unfulfill!") { expect(instance).to respond_to(:unfulfill!).with(0).argument }
+  end
+end
