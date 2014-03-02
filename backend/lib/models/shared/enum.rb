433881976c7a3fb39706b1d75f35154a385bd8f6
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-03-01 21:59:24 -0800

    Add Enum module

---
+++ backend/lib/models/shared/enum.rb
@@ -0,0 +1,26 @@
+module Enum
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+
+    def enum(field, *values)
+      values.flatten!
+      const_name = "#{field}".upcase
+      const_set(
+        const_name, 
+        values.reduce({}) do |hash,status| 
+          hash[status.to_sym] = values.index(status)
+          hash
+        end
+      )
+
+      values.each do |status|
+        define_method "#{status}?" do
+          send(field.to_sym) == self.class.const_get(const_name)[status.to_sym]
+        end
+      end
+    end
+
+  end
+
+end
