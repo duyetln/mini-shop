4cab373162bb93fc84f146cf20f13a5bccfbf8a7
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-03-02 18:09:36 -0800

    Add additional validations to Discount class

--- backend/lib/models/inventory/discount.rb
+++ backend/lib/models/inventory/discount.rb
@@ -6,18 +6,20 @@ class Discount < ActiveRecord::Base
   validates :rate, numericality: { greater_than_or_equal_to: 0 }
   validates :name, uniqueness: true
 
-  def rate_at(datetime=DateTime.now)
+  validate :discount_dates
+
+  def rate_at(dt=DateTime.now)
     zero_rate = BigDecimal.new("0.0")
     start_set = start_at.present?
     end_set   = end_at.present?
     
     case
     when  start_set &&  end_set
-      start_at <= datetime && datetime <= end_at ? rate : zero_rate
+      start_at <= dt && dt <= end_at ? rate : zero_rate
     when  start_set && !end_set
-      start_at <= datetime ? rate : zero_rate
+      start_at <= dt ? rate : zero_rate
     when !start_set &&  end_set then
-      datetime <= end_at ? rate : zero_rate
+      dt <= end_at ? rate : zero_rate
     when !start_set && !end_set
       rate
     end
@@ -28,5 +30,14 @@ class Discount < ActiveRecord::Base
   end
 
   alias_method :current_rate, :rate_at
+
+  protected
+
+  def discount_dates
+    unless ( start_at.present? && end_at.present? ? start_at <= end_at : true )
+      errors.add(:start_at, "must be before or same as end_at")
+      errors.add(:end_at, "must be after or same as start_at")
+    end
+  end
 
 end
