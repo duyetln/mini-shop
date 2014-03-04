20cb80aae8049b3befa381aef75583c834f5bdd4
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-03-03 23:06:53 -0800

    Update User factory

--- backend/spec/factories/accounts/users.rb
+++ backend/spec/factories/accounts/users.rb
@@ -1,18 +1,10 @@
 FactoryGirl.define do
   factory :user do
     
-    first_name "John"
-    last_name  "Smith"
-    email      "john.smith@email.com"
-    birthdate  Date.today
-    password   "password"
-
-    trait :actv_code_blank do
-      actv_code nil
-    end
-
-    trait :actv_code_present do
-      actv_code SecureRandom.hex(5)
-    end
+    first_name { Faker::Name.first_name }
+    last_name  { Faker::Name.last_name }
+    email      { Faker::Internet.email( first_name + last_name ) }
+    birthdate  { 23.years.ago }
+    password   { Faker::Lorem.characters(20) }
   end
 end
