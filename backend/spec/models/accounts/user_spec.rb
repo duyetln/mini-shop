83685d45e0e49101071c330f81e52e9d2314aeca
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-03-04 12:50:35 -0800

    Update User spec

--- backend/spec/models/accounts/user_spec.rb
+++ backend/spec/models/accounts/user_spec.rb
@@ -1,45 +1,73 @@
 require "spec_helper"
 
-describe User do 
+describe User do
+
+  it { should allow_mass_assignment_of(:first_name) }
+  it { should allow_mass_assignment_of(:last_name) }
+  it { should allow_mass_assignment_of(:email) }
+  it { should allow_mass_assignment_of(:birthdate) }
+  it { should allow_mass_assignment_of(:password) }
+
+  it { should validate_presence_of(:first_name) }
+  it { should validate_presence_of(:last_name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:birthdate) }
+  it { should validate_presence_of(:password) }
+
+  it { should validate_uniqueness_of(:email) }
+  it { should ensure_length_of(:password).is_at_least(5) }
+  it { should have_readonly_attribute(:uuid) }
 
   describe "factory model" do
 
-    it("is valid")           { expect(built_user.valid?).to be_true }
+    it("is valid") { expect(built_user.valid?).to be_true }
     it("saves successfully") { expect(created_user).to be_present }
+  end
+
+  let(:password) { built_user.password }
+  let :user do
+    built_user.password = password
+    built_user.save
+    built_user
   end
 
   describe "#save" do
 
-    context("blank first_name") { it("does not save") { built_user.first_name  = nil; expect(built_user.save).to be_false } }
-    context("blank last_name")  { it("does not save") { built_user.last_name   = nil; expect(built_user.save).to be_false } }
-    context("blank birthdate")  { it("does not save") { built_user.birthdate   = nil; expect(built_user.save).to be_false } }
-    context("blank email")      { it("does not save") { built_user.email       = nil; expect(built_user.save).to be_false } }
-    context("blank password")   { it("does not save") { built_user.password    = nil; expect(built_user.save).to be_false } }
-    
-    context "valid input" do
+    context "new user" do
 
-      before(:each) { @password = built_user.password; expect(built_user.save).to eq(true) }
+      it("sets uuid") { expect(created_user.uuid).to be_present } 
+      it("sets actv_code") { expect(created_user.actv_code).to be_present }
+      it("sets password") { expect(created_user.password).to be_present }
 
-      it("sets uuid")              { expect(built_user.uuid).to be_present } 
-      it("sets actv_code") { expect(built_user.actv_code).to be_present }
-      it("sets password")          { expect(built_user.password).to be_present }
-      it("encrypts password")      { expect(BCrypt::Password.new(built_user.password)).to eq(@password) }
+      it "encrypts password" do 
+        
+        expect(BCrypt::Password.new(user.password)).to eq(password)
+      end
     end
 
-    context "password changed" do
+    context "created user" do
 
-      it "encrypts password and saves" do
+      it("doesn't change uuid") { expect{ created_user.save }.to_not change{ created_user.uuid } }
 
-        created_user.password = random_string
-        created_user.save
-        expect(BCrypt::Password.new(created_user.password)).to eq(random_string)
+      context "password changed" do
+
+        it "encrypts new password" do
+
+          new_password = Faker::Lorem.characters(20)
+          created_user.password = new_password
+          expect(created_user.save).to be_true
+          expect(BCrypt::Password.new(created_user.password)).to eq(new_password)
+        end
       end
     end
   end
 
   describe "#confirmed?" do
 
-    context("new user") { it("returns false") { expect(built_user.confirmed?).to be_false } }
+    context "new user" do 
+
+      it("returns false") { expect(built_user.confirmed?).to be_false }
+    end
 
     context "persisted user" do
 
@@ -48,17 +76,24 @@ describe User do
         it "returns true" do
 
           created_user.actv_code = nil
-          created_user.save; expect(created_user.confirmed?).to be_true
+          created_user.save;
+          expect(created_user.confirmed?).to be_true
         end
       end
 
-      context("confirmation code present") { it("returns false") { expect(created_user.confirmed?).to be_false } }
+      context "confirmation code present" do
+
+        it("returns false") { expect(created_user.confirmed?).to be_false }
+      end
     end
   end
 
   describe "#confirm!" do
 
-    context("new user") { it("returns false") { expect(built_user.confirm!).to be_false } }
+    context "new user" do
+
+      it("returns false") { expect(built_user.confirm!).to be_false }
+    end
     
     context "persisted user" do
 
@@ -74,9 +109,14 @@ describe User do
 
       context "confirmation code present" do 
 
-        it("should return true")  do
+        it "returns true" do
 
           expect(created_user.confirm!).to be_true
+        end
+
+        it "clears the activation code" do
+
+          created_user.confirm!
           expect(created_user.actv_code).to be_blank
         end
       end
@@ -85,20 +125,27 @@ describe User do
 
   describe ".authenticate" do
 
-    before :each do
-      @password = built_user.password
-      built_user.save
+    context "non-matching uuid" do
+
+      it("returns nil") { expect(User.authenticate(random_string, random_string)).to be_nil }
     end
 
-    context("non-matching uuid")     { it("returns nil") { expect(User.authenticate(built_user.uuid + random_string, random_string)).to be_nil } }
-    context("unconfirmed user")      { it("returns nil") { expect(User.authenticate(built_user.uuid, built_user.password)).to be_nil } }
-    context("non-matching password") { it("returns nil") { expect(User.authenticate(built_user.uuid, built_user.password + random_string)).to be_nil } }
+    context "unconfirmed user" do
+
+      it("returns nil") { expect(User.authenticate(user.uuid, password)).to be_nil }
+    end
+
+    context "non-matching password" do
+
+      it("returns nil") { expect(User.authenticate(user.uuid, random_string)).to be_nil }
+    end
+
     context "matching uuid, matching password, confirmed user" do
 
       it "returns the user" do
         
-        built_user.confirm!
-        expect(User.authenticate(built_user.uuid, @password)).to eq(built_user)
+        user.confirm!
+        expect(User.authenticate(user.uuid, password)).to eq(user)
       end
     end
   end
