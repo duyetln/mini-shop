6b153d016ba5a5674381b0be436c2b7f75a2ba78
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-03-05 00:46:55 -0800

    Add Itemable module

---
+++ backend/lib/models/shared/itemable.rb
@@ -0,0 +1,29 @@
+module Itemable
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+
+    def add_or_update(item, qty=1, acc=true, conds={})
+      record = where({
+        item_type: item.class, 
+        item_id: item.id
+      }.merge(conds)).first_or_initialize
+
+      record.quantity ||= 0
+      acc ? 
+        record.quantity += qty.to_i : 
+        record.quantity  = qty.to_i
+
+      yield record if block_given?
+      record.save! ? record : nil
+    end
+
+    def get(item)
+      record = detect{ |r| r.item == item } || detect{ |r| r == item }
+      yield record if block_given?
+      record
+    end
+
+  end
+
+end
