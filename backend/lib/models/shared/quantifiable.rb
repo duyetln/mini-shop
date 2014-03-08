085e63983b0af8ec06463a1f9f7210eff87c5a2f
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-03-07 16:43:05 -0800

    Add Quantifiable module

---
+++ backend/lib/models/shared/quantifiable.rb
@@ -0,0 +1,29 @@
+module Quantifiable
+
+  extend ActiveSupport::Concern
+
+  included do
+
+    attr_accessible :quantity
+
+    validates :quantity, presence: true
+    validates :quantity, numericality: { greater_than_or_equal_to: 0 }
+  end
+
+  [:quantity].each do |method|
+    class_eval <<-EOF
+      def #{method}(*args)
+        defined?(super) ? super : ( raise "#{__method__} must be defined in derived class" )
+      end
+    EOF
+  end
+
+  [:quantity=].each do |method|
+    class_eval <<-EOF
+      def #{method}(*args)
+        defined?(super) ? super : ( raise "#{__method__} must be defined in derived class" )
+      end
+    EOF
+  end
+  
+end
