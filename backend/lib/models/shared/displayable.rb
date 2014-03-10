847cbaedc6813eddff6e9083ded374c008e69ee1
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-03-10 12:05:03 -0700

    Displayable items need not to have title, description attributes

--- backend/lib/models/shared/displayable.rb
+++ backend/lib/models/shared/displayable.rb
@@ -4,20 +4,12 @@ module Displayable
 
   included do
 
-    attr_accessible :title, :description
+    attr_accessible :title, :description  if ( [:title, :description] - self.column_names.map(&:to_sym) ).blank?
 
     validates :title, presence: true
   end
 
   [:title, :description].each do |method|
-    class_eval <<-EOF
-      def #{method}(*args)
-        defined?(super) ? super : ( raise "#{__method__} must be defined in derived class" )
-      end
-    EOF
-  end
-
-  [:title=, :description=].each do |method|
     class_eval <<-EOF
       def #{method}(*args)
         defined?(super) ? super : ( raise "#{__method__} must be defined in derived class" )
--- backend/spec/models/shared/displayable.rb
+++ backend/spec/models/shared/displayable.rb
@@ -1,16 +1,16 @@
 shared_examples "displayable object" do
 
-  it { should allow_mass_assignment_of(:title) }
-  it { should allow_mass_assignment_of(:description) }
+  if ( [:title, :description] - described_class.column_names.map(&:to_sym) ).blank?
+    it { should allow_mass_assignment_of(:title) }
+    it { should allow_mass_assignment_of(:description) }
+  end
 
   it { should validate_presence_of(:title) }
 
   context "instance" do
     let(:subject) { described_class.new }
     it { should respond_to(:title).with(0).argument }
-    it { should respond_to(:title=).with(1).argument }
     it { should respond_to(:description).with(0).argument }
-    it { should respond_to(:description=).with(1).argument }
   end
 
 end
