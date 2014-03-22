2a876a531345061a4efab9d20a492b8618563d75
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-03-22 13:15:41 -0700

    Add Address factory

---
+++ backend/spec/factories/shopping/addresses.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :address do
+    line1 { Faker::Address.street_address }
+    city { Faker::Address.city }
+    region { Faker::Address.state }
+    postal_code { Faker::Address.postcode }
+    country { Faker::Address.country }
+    association :user
+  end
+end
