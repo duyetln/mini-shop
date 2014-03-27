44ebe562fcb0421e5d553843b5ad2695902b30c7
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-03-26 23:31:10 -0700

    Bug fixes + some updates to ItemCombinable interface

--- backend/lib/models/shared/item_combinable.rb
+++ backend/lib/models/shared/item_combinable.rb
@@ -9,23 +9,23 @@ module ItemCombinable
 
   module ClassMethods
 
-    def add_or_update(item, qty=1, acc=true, conds={})
+    def add_or_update(item, qty=1, acc=true, conds={}, attrs={})
       record = where({
         item_type: item.class, 
         item_id: item.id
-      }.merge(conds)).first_or_initialize
+      }.merge(conds)).first_or_initialize(attrs)
 
       record.quantity ||= 0
       acc ? 
-        record.quantity += qty.to_i : 
-        record.quantity  = qty.to_i
+        record.quantity += qty : 
+        record.quantity  = qty
 
       yield record if block_given?
       record.save! ? record : nil
     end
 
     def get(item)
-      record = detect{ |r| r.item == item } || detect{ |r| r == item }
+      record = all.detect{ |r| r.item == item } || detect{ |r| r == item }
       yield record if block_given?
       record
     end
