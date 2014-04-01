0b9c3e64e88d7fe08b571e0985c9ed0befb15cd7
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-04-01 01:09:58 -0700

    Make Purchase factory have ready trait by default

--- backend/spec/factories/shopping/orders.rb
+++ backend/spec/factories/shopping/orders.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :order do
-    purchase { build :purchase, :ready }
+    purchase { build :purchase }
     item { build :storefront_item }
     qty { rand(1..10) }
     currency { build :usd }
--- backend/spec/factories/shopping/purchases.rb
+++ backend/spec/factories/shopping/purchases.rb
@@ -1,11 +1,14 @@
 FactoryGirl.define do
   factory :purchase do
     user { build :user }
+    payment_method { build :payment_method }
+    billing_address { build :address }
+    shipping_address { build :address }
 
-    trait :ready do
-      payment_method { build :payment_method }
-      billing_address { build :address }
-      shipping_address { build :address }
+    trait :orders do
+      after :build do |purchase|
+        purchase.orders << build(:order)
+      end
     end
   end
 end
