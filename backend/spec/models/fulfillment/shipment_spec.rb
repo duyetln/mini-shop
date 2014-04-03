8d606ccd07de954141b4fe3ce65932ba9a4f73e0
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-04-02 18:53:39 -0700

    Add more specs to Shipment specs

--- backend/spec/models/fulfillment/shipment_spec.rb
+++ backend/spec/models/fulfillment/shipment_spec.rb
@@ -3,6 +3,19 @@ require 'spec/models/shared/item_combinable'
 
 describe Shipment do
 
-  # it_behaves_like "item combinable model"
+  it_behaves_like "item combinable model"
 
+  it { should have_readonly_attribute(:user_id) }
+  it { should have_readonly_attribute(:order_id) }
+  it { should have_readonly_attribute(:shipping_address_id) }
+
+  it { should belong_to(:user) }
+  it { should belong_to(:order) }
+  it { should belong_to(:shipping_address).class_name('Address') }
+
+  it { should validate_presence_of(:user) }
+  it { should validate_presence_of(:order) }
+  it { should validate_presence_of(:shipping_address) }
+
+  it { should ensure_inclusion_of(:item_type).in_array(%w{ PhysicalItem }) }
 end
