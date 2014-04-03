a213673b49364d1582b9f81fb760e294b7c7f1b5
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-04-02 18:56:29 -0700

    Add more specs to Ownership specs

--- backend/spec/models/fulfillment/ownership_spec.rb
+++ backend/spec/models/fulfillment/ownership_spec.rb
@@ -3,6 +3,17 @@ require 'spec/models/shared/item_combinable'
 
 describe Ownership do
 
-  # it_behaves_like "item combinable model"
+  it_behaves_like "item combinable model"
+
+  it { should have_readonly_attribute(:user_id) }
+  it { should have_readonly_attribute(:order_id) }
+
+  it { should belong_to(:user) }
+  it { should belong_to(:order) }
+
+  it { should validate_presence_of(:user) }
+  it { should validate_presence_of(:order) }
+
+  it { should ensure_inclusion_of(:item_type).in_array(%w{ DigitalItem }) }
 
 end
