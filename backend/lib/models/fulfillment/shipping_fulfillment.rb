ba43a702840a5cea7117328093a5ff73ecf986e2
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-04-02 17:45:32 -0700

    Use Ownership.add_or_update in ShippingFulfillment#process_fulfillment!

--- backend/lib/models/fulfillment/shipping_fulfillment.rb
+++ backend/lib/models/fulfillment/shipping_fulfillment.rb
@@ -2,16 +2,9 @@ class ShippingFulfillment < Fulfillment
   protected
 
   def process_fulfillment!
-    shipment = Shipment.where(
-      order_id: order.id,
-      item_type: item.class,
-      item_id: item.id
-    ).first_or_initialize
-
-    shipment.user = order.user
-    shipment.qty ||= 0
-    shipment.qty  += 1
-    shipment.shipping_address = order.shipping_address
-    shipment.save!
+    Shipment.add_or_update(item, conds: { order_id: order.id }) do |shipment|
+      shipment.user = order.user
+      shipment.shipping_address = order.shipping_address
+    end
   end
 end
