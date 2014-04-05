a62fd6fbc19b9978fdb3fad50e514971c53bfbf3
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-04-04 23:23:12 -0700

    Add ShippingFulfillment spec

---
+++ backend/spec/models/fulfillment/shipping_fulfillment_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe ShippingFulfillment do
+
+  it { should ensure_inclusion_of(:item_type).in_array(%w{ PhysicalItem }) }
+
+  describe '#process_fulfillment!' do
+    let(:shipment) { FactoryGirl.build :shipment, order: model.order, item: model.item }
+
+    before :each do
+      expect(Shipment).to receive(:add_or_update).with(
+        model.item,
+        conds: { order_id: model.order.id }
+      ).and_yield(shipment)
+    end
+
+    it 'adds or updates shipment for the same order' do
+      model.send(:process_fulfillment!)
+    end
+
+    it 'changes the shipment user' do
+      expect { model.send(:process_fulfillment!) }.to change { shipment.user }.to(model.order.user)
+    end
+
+    it 'changes the shipment shipping address' do
+      expect { model.send(:process_fulfillment!) }.to change { shipment.shipping_address }.to(model.order.shipping_address)
+    end
+  end
+end
