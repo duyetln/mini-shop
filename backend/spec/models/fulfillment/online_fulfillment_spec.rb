372af5367b48e6ea4af9240b9357c712b33585a9
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-04-04 23:05:16 -0700

    Add OnlineFulfillment spec

---
+++ backend/spec/models/fulfillment/online_fulfillment_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe OnlineFulfillment do
+
+  it { should ensure_inclusion_of(:item_type).in_array(%w{ DigitalItem }) }
+
+  describe '#process_fulfillment!' do
+    let(:ownership) { FactoryGirl.build :ownership, order: model.order, item: model.item }
+
+    before :each do
+      expect(Ownership).to receive(:add_or_update).with(
+        model.item,
+        conds: { order_id: model.order.id }
+      ).and_yield(ownership)
+    end
+
+    it 'adds or updates ownership for the same order' do
+      model.send(:process_fulfillment!)
+    end
+
+    it 'changes the ownership user' do
+      expect { model.send(:process_fulfillment!) }.to change { ownership.user }.to(model.order.user)
+    end
+  end
+end
