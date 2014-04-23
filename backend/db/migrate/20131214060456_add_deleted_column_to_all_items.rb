8c669c5b266545ea0418f17dfdf06b055b3bd76a
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-04-23 00:54:12 -0700

    Rename add_removed_column_to_all items

--- backend/db/migrate/20131214060456_add_deleted_column_to_all_items.rb
+++ backend/db/migrate/20131214060456_add_deleted_column_to_all_items.rb
@@ -1,4 +1,4 @@
-class AddRemovedColumnToAllItems < ActiveRecord::Migration
+class AddDeletedColumnToAllItems < ActiveRecord::Migration
   def change
     [:physical_items, :digital_items, :bundle_items, :storefront_items].each do |table|
       add_column table, :deleted, :boolean
