63a5effd783bf94a10df0512ac9cc3dbe9049a85
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-04-30 18:28:30 -0700

    Add Services::Helpers helper

---
+++ backend/lib/services/shared/helpers.rb
@@ -0,0 +1,25 @@
+module Services
+  module Helpers
+    protected
+
+    def process_request
+      yield
+    rescue ActiveRecord::RecordNotFound => ex
+      fail ::Services::Errors::NotFound, ex.message
+    rescue ActiveRecord::RecordInvalid => ex
+      fail ::Services::Errors::BadRequest, ex.message
+    rescue => ex
+      fail ::Services::Errors::ServerError, ex.message
+    end
+
+    def respond_with(body)
+      yield body if block_given?
+      respond_to do |format|
+        format.js { body.to_json }
+        format.json { body.to_json }
+        format.xml { 'XML Representation Not Supported' }
+        format.html { 'HTML Representation Not Supported' }
+      end
+    end
+  end
+end
