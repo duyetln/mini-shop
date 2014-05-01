c8979a5772d7587e5b33d018961d04f823e498b7
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-04-30 18:52:32 -0700

    Update Services::Base

--- backend/lib/services/base.rb
+++ backend/lib/services/base.rb
@@ -1,48 +1,19 @@
+require 'services/shared/errors'
+require 'services/shared/helpers'
+require 'sinatra/namespace'
+
 module Services
   class Base < Sinatra::Base
     register Sinatra::RespondTo
+    register Sinatra::Namespace
+    helpers Services::Errors
+    helpers Services::Helpers
 
     configure do
       set :default_content, :json
       disable :show_exceptions
       disable :raise_errors
-    end
-
-    error ActiveRecord::RecordNotFound do
-      status 404
-      respond_with message: exception.message
-    end
-
-    error ActiveRecord::RecordInvalid do
-      status 400
-      respond_with messages: exception.message, errors: exception.record.errors
-    end
-
-    not_found do
-      respond_with message: 'Resource Not Found'
-    end
-
-    error do
-      respond_with message: exception.message
-    end
-
-    helpers do
-
-      protected
-
-      def exception
-        env['sinatra.error']
-      end
-
-      def respond_with(body)
-        yield body if block_given?
-        respond_to do |format|
-          format.js { body.to_json }
-          format.json { body.to_json }
-          format.xml { 'XML Representation Not Supported' }
-          format.html { 'HTML Representation Not Supported' }
-        end
-      end
+      disable :dump_errors
     end
   end
 end
