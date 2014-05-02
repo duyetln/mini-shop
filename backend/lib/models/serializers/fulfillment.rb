ece5655c477d8f5ed9a067747f2e90d255153884
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-05-02 00:25:18 -0700

    Add serializers for fulfillment models

---
+++ backend/lib/models/serializers/fulfillment.rb
@@ -0,0 +1,13 @@
+require 'models/serializers/base'
+require 'models/serializers/shopping'
+
+class OwnershipSerializer < ResourceSerializer
+  attributes :user_id, :order_id, :item_type, :item_id, :qty, :deleted, :created_at
+  has_one :item, serializer: DynamicSerializer
+end
+
+class ShipmentSerializer < ResourceSerializer
+  attributes :user_id, :order_id, :item_type, :item_id, :qty, :shipping_address_id, :created_at
+  has_one :item, serializer: DynamicSerializer
+  has_one :shipping_address, serializer: AddressSerializer
+end
