ceb2901b8b05174aa8fab30f5ffb3294d7029e1b
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-05-03 16:37:22 -0700

    Update user service

--- backend/lib/services/accounts/users_svc.rb
+++ backend/lib/services/accounts/users_svc.rb
@@ -1,64 +1,84 @@
-class UsersSvc < Sinatra::Base
-  before { content_type :json }
+require 'services/base'
+require 'models/serializers/accounts'
 
-  helpers do
+module Services
+  module Accounts
+    class Users < Services::Base
+      get '/users/:id' do
+        process_request do
+          user = User.find(params[:id])
+          respond_with(UserSerializer.new(user))
+        end
+      end
 
-    protected
+      post '/users' do
+        process_request do
+          user = User.new(params[:user])
+          user.save!
+          respond_with(UserSerializer.new(user))
+        end
+      end
 
-    def find_user_by_id!
-      @user = User.find_by_id(params[:id]) || halt(404)
+      post '/users/authenticate' do
+        process_request do
+          user = User.authenticate!(
+            params[:email],
+            params[:password]
+          ) || unauthorized!
+          respond_with(UserSerializer.new(user))
+        end
+      end
+
+      put '/users/:id' do
+        process_request do
+          user = User.find(params[:id])
+          user.update_attributes!(params[:user])
+          respond_with(UserSerializer.new(user))
+        end
+      end
+
+      put '/users/:uuid/confirm/:actv_code' do
+        process_request do
+          user = User.confirm!(params[:uuid], params[:actv_code])
+          respond_with(UserSerializer.new(user))
+        end
+      end
+
+      get '/users/:id/purchases' do
+        process_request do
+          purchases = User.find(params[:id]).purchases
+          respond_with(purchases.map do |purchase|
+            PurchaseSerializer.new(purchase)
+          end)
+        end
+      end
+
+      get '/users/:id/orders' do
+        process_request do
+          orders = User.find(params[:id]).purchases.map(&:orders).flatten
+          respond_with(orders.map do |order|
+            OrderSerializer.new(order)
+          end)
+        end
+      end
+
+      get '/users/:id/ownerships' do
+        process_request do
+          user = User.find(params[:id])
+          respond_with(user.ownerships.map do |ownership|
+            OwnershipSerializer.new(ownership)
+          end)
+        end
+      end
+
+      get '/users/:id/shipments' do
+        process_request do
+          user = User.find(params[:id])
+          respond_with(user.shipments.map do |shipment|
+            ShipmentSerializer.new(shipment)
+          end)
+        end
+      end
     end
-
-    def find_user_by_uuid!
-      @user = User.find_by_uuid(params[:uuid]) || halt(404)
-    end
-
-    def user_response_options
-      { root: false, except: [:password, :updated_at] }
-    end
-
-    def load_user!
-      @user = User.new(accessible_params)
-      @user.valid? && @user || halt(400)
-    end
-
-    def accessible_params
-      params.slice(*User.column_names)
-    end
-
-    def respond_with(resource, response_options = {}, json_options = user_response_options)
-      resource.present? || halt(404)
-      status = block_given? ? yield(resource) : resource
-      success_code = response_options[:success] || 200
-      failure_code = response_options[:failure] || 500
-      status ? halt(success_code, resource.to_json(json_options)) : halt(failure_code)
-    end
-
-  end
-
-  get '/users/:id' do
-    find_user_by_id!
-    respond_with(@user)
-  end
-
-  post '/users' do
-    load_user!
-    respond_with(@user) { |c| c.save }
-  end
-
-  post '/users/authenticate' do
-    @user = User.authenticate(params[:uuid], params[:password])
-    respond_with(@user)
-  end
-
-  put '/users/:id' do
-    find_user_by_id!
-    respond_with(@user, failure: 400) { |c| c.update_attributes(accessible_params) }
-  end
-
-  put '/users/:uuid/confirm/:actv_code' do
-    find_user_by_uuid!
-    !@user.confirmed? && @user.actv_code == params[:actv_code] || halt(404)
-    respond_with(@user) { |c| c.confirm! }
   end
 end
