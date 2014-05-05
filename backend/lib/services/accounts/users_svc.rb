fdc35964812fb1926d016189ad4e17eecbd44707
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-05-04 22:41:27 -0700

    Add endpoints to get/create/update user's payment methods

--- backend/lib/services/accounts/users_svc.rb
+++ backend/lib/services/accounts/users_svc.rb
@@ -68,6 +68,30 @@ module Services
         end
       end
 
+      get '/users/:id/payment_methods' do
+        process_request do
+          payment_methods = User.find(params[:id]).payment_methods
+          respond_with(payment_methods.map do |payment_method|
+            PaymentMethodSerializer.new(payment_method)
+          end)
+        end
+      end
+
+      post '/users/:id/payment_methods' do
+        process_request do
+          payment_method = User.find(params[:id]).payment_methods.create!(params[:payment_method])
+          respond_with(PaymentMethodSerializer.new(payment_method))
+        end
+      end
+
+      put '/users/:id/payment_methods/:payment_method_id' do
+        process_request do
+          payment_method = User.find(params[:id]).payment_methods.find(params[:payment_method_id])
+          payment_method.update_attributes!(params[:payment_method])
+          respond_with(PaymentMethodSerializer.new(payment_method))
+        end
+      end
+
       get '/users/:id/purchases' do
         process_request do
           purchases = User.find(params[:id]).purchases
--- backend/spec/services/accounts/users_svc_spec.rb
+++ backend/spec/services/accounts/users_svc_spec.rb
@@ -250,6 +250,96 @@ describe Services::Accounts::Users do
     end
   end
 
+  describe 'get /users/:id/payment_methods' do
+    let(:method) { :get }
+    let(:path) { "/users/#{id}/payment_methods" }
+
+    context 'invalid id' do
+      let(:id) { rand_str }
+
+      include_examples 'not found'
+    end
+
+    context 'valid id' do
+      it 'returns the payment methods' do
+        send_request
+        expect_status(200)
+        expect_response(user.payment_methods.map do |payment_method|
+          PaymentMethodSerializer.new(payment_method)
+        end.to_json)
+      end
+    end
+  end
+
+  describe 'post /users/:id/payment_methods' do
+    let(:method) { :post }
+    let(:path) { "/users/#{id}/payment_methods" }
+
+    context 'invalid id' do
+      let(:id) { rand_str }
+
+      include_examples 'not found'
+    end
+
+    context 'valid id' do
+      context 'invalid parameters' do
+        let(:params) { {} }
+
+        include_examples 'bad request'
+      end
+
+      context 'valid parameters' do
+        let :params do
+          { payment_method: FactoryGirl.build(:payment_method).attributes.except('user_id') }
+        end
+
+        it 'creates a new payment method' do
+          expect { send_request }.to change { user.payment_methods.count }.by(1)
+          expect_status(200)
+          expect_response(PaymentMethodSerializer.new(user.payment_methods.last).to_json)
+        end
+      end
+    end
+  end
+
+  describe 'put /users/:id/payment_methods/:payment_method_id' do
+    let(:method) { :put }
+    let(:path) { "/users/#{id}/payment_methods/#{payment_method_id}" }
+    let(:payment_method) { PaymentMethod.find FactoryGirl.create(:payment_method, user: user).id }
+    let(:payment_method_id) { payment_method.id }
+    let(:params) { { payment_method: payment_method.attributes } }
+
+    context 'invalid id' do
+      let(:id) { rand_str }
+
+      include_examples 'not found'
+    end
+
+    context 'valid id' do
+      context 'invalid payment method id' do
+        let(:payment_method_id) { rand_str }
+
+        include_examples 'not found'
+      end
+
+      context 'valid payment method id' do
+        context 'invalid parameters' do
+          let(:params) { { payment_method: { balance: nil } } }
+
+          include_examples 'bad request'
+        end
+
+        context 'valid parameters' do
+          it 'updates the payment method' do
+            send_request
+            expect_status(200)
+            expect_response(PaymentMethodSerializer.new(payment_method).to_json)
+          end
+        end
+      end
+    end
+  end
+
   describe 'get /users/:id/purchases' do
     let(:method) { :get }
     let(:path) { "/users/#{id}/purchases" }
