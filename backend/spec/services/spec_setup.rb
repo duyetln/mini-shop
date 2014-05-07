d2b369691681710af9ea3df9ac4c3fd0df62c392
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-05-07 00:11:44 -0700

    Clean db after each service spec example

--- backend/spec/services/spec_setup.rb
+++ backend/spec/services/spec_setup.rb
@@ -36,4 +36,12 @@ Application.load_services!
 
 RSpec.configure do |config|
   config.include SpecHelpers::Services
+
+  config.before :each do
+    DatabaseCleaner.start
+  end
+
+  config.after :each do
+    DatabaseCleaner.clean
+  end
 end
