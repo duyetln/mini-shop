7e069454f287a9dda173a14ee6a025ee6082ca11
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-05-09 00:21:45 -0700

    Add endpoints to get/create prices

---
+++ backend/lib/services/inventory/prices.rb
@@ -0,0 +1,33 @@
+require 'services/base'
+require 'models/serializers/inventory'
+
+module Services
+  module Inventory
+    class Prices < Services::Base
+      get '/prices' do
+        process_request do
+          prices = Price.all
+          respond_with(prices.map do |price|
+            PriceSerializer.new(price)
+          end)
+        end
+      end
+
+      post '/prices' do
+        process_request do
+          price = Price.new(params[:price])
+          price.save!
+          respond_with(PriceSerializer.new(price))
+        end
+      end
+
+      put '/prices/:id' do
+        process_request do
+          price = Price.find(params[:id])
+          price.update_attributes!(params[:price])
+          respond_with(PriceSerializer.new(price))
+        end
+      end
+    end
+  end
+end
---
+++ backend/spec/services/inventory/prices_spec.rb
@@ -0,0 +1,78 @@
+require 'services/spec_setup'
+require 'spec/services/shared/errors'
+
+describe Services::Inventory::Prices do
+  describe 'get /prices' do
+    let(:method) { :get }
+    let(:path) { '/prices' }
+
+    before :each do
+      FactoryGirl.create :price
+    end
+
+    it 'returns all prices' do
+      send_request
+      expect_status(200)
+      expect_response(Price.all.map do |price|
+        PriceSerializer.new(price)
+      end.to_json)
+    end
+  end
+
+  describe 'post /prices' do
+    let(:method) { :post }
+    let(:path) { '/prices' }
+
+    context 'invalid parameters' do
+      let(:params) { {} }
+
+      include_examples 'bad request'
+    end
+
+    context 'valid parameters' do
+      let :params do
+        { 
+          price: FactoryGirl.build(
+            :price,
+            pricepoint: FactoryGirl.create(:pricepoint)
+          ).attributes
+        }
+      end
+
+      it 'creates new price' do
+        expect { send_request }.to change { Price.count }.by(1)
+        expect_status(200)
+        expect_response(PriceSerializer.new(Price.last).to_json)
+      end
+    end
+  end
+
+  describe 'put /prices/:id' do
+    let(:method) { :put }
+    let(:path) { "/prices/#{id}" }
+    let(:id) { rand_str }
+
+    include_examples 'invalid id'
+
+    context 'valid id' do
+      let(:price) { FactoryGirl.create :price }
+      let(:id) { price.id }
+
+      context 'invalid parameters' do
+        let(:params) { { price: { pricepoint_id: nil } } }
+
+        include_examples 'bad request'
+      end
+
+      context 'valid parameters' do
+        let(:params) { { price: price.attributes } }
+
+        it 'updates the existing price' do
+          send_request
+          expect_status(200)
+          expect_response(PriceSerializer.new(price).to_json)
+        end
+      end
+    end
+  end
+end
