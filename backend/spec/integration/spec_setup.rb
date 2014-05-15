943cde77cbaf0e3cd7b6f226e998b93366c96009
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-05-14 18:37:48 -0700

    Move SpecHelpers::Services to spec/spec_setup

--- backend/spec/integration/spec_setup.rb
+++ backend/spec/integration/spec_setup.rb
@@ -1,7 +1,11 @@
 require 'spec_setup'
 
+Application.load_services!
+
 RSpec.configure do |config|
+  config.include SpecHelpers::Services
   config.order = 'default'
+
   config.before :all do
     DatabaseCleaner.start
   end
--- backend/spec/services/spec_setup.rb
+++ backend/spec/services/spec_setup.rb
@@ -1,37 +1,5 @@
 require 'spec_setup'
 
-module SpecHelpers
-  module Services
-    extend ActiveSupport::Concern
-    include Rack::Test::Methods
-
-    included do
-      let(:params) { {} }
-      let(:response_status) { last_response.status }
-      let(:response_body) { last_response.body }
-      let :parsed_response do
-        Yajl::Parser.parse(last_response.body, symbolize_keys: true)
-      end
-    end
-
-    def app
-      described_class
-    end
-
-    def expect_status(code)
-      expect(response_status).to eq(code)
-    end
-
-    def expect_response(body)
-      expect(response_body).to eq(body)
-    end
-
-    def send_request
-      send method, path, params
-    end
-  end
-end
-
 Application.load_services!
 
 RSpec.configure do |config|
