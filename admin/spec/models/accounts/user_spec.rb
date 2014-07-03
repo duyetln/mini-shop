8f03768473c535c29ef9cfe2c4d412105a62b3ce
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-07-03 00:54:28 -0700

    Add initial user service resource spec

---
+++ admin/spec/models/accounts/user_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_setup'
+require 'models/service_resource'
+
+describe User do
+  include_examples 'service resource'
+  include_examples 'default all'
+  include_examples 'default find'
+  include_examples 'default create'
+  include_examples 'default update'
+
+  describe '.instantiate' do
+    let(:model) { described_class.instantiate(parse(resource_payload)) }
+
+    it 'sets addresses correctly' do
+      expect(model.addresses).to contain_exactly(
+        an_instance_of(Address),
+        an_instance_of(Address)
+      )
+    end
+
+    it 'sets payment methods correctly' do
+      expect(model.payment_methods).to contain_exactly(an_instance_of(PaymentMethod))
+    end
+  end
+end
