c5cabb5fa803e1c568eb39331e02184cbd34de81
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-07-05 20:27:53 -0700

    Update Gemfile, boot.rb, add pry rake task, fix style

--- shared/boot.rb
+++ shared/boot.rb
@@ -1,4 +1,5 @@
 env = (ENV['APP_ENV'] ||= 'development')
 Bundler.require :default, env.to_sym
-$LOAD_PATH.unshift File.expand_path File.dirname(__FILE__)
+$LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__))
+$LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + '/lib')
 Dir['lib/**/*.rb'].each { |f| require f }
