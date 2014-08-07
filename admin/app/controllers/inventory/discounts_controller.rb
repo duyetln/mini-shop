01c9d6ebe858eaaa3d64faa26576ed723cb78481
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-08-06 19:25:19 -0700

    Add discount index page

--- admin/app/controllers/inventory/discounts_controller.rb
+++ admin/app/controllers/inventory/discounts_controller.rb
@@ -2,19 +2,40 @@ module Inventory
   class DiscountsController < ApplicationController
     def index
       @discounts = resource_class.all(pagination)
-      render nothing: true
     end
 
     def create
       @discount = resource_class.create(
         scoped_params(:discount, :name, :rate, :start_at, :end_at)
       )
-      render nothing: true
     end
 
     def update
+      if params[:discount].present?
+        begin
+          if params[:discount][:start_at].present?
+            params[:discount][:start_at] = DateTime.strptime(
+              params[:discount][:start_at],
+              '%m/%d/%Y %H:%M'
+            )
+          end
+
+          if params[:discount][:end_at].present?
+            params[:discount][:end_at] = DateTime.strptime(
+              params[:discount][:end_at],
+              '%m/%d/%Y %H:%M'
+            )
+          end
+        rescue ArgumentError
+          flash[:error] = {
+            primary: 'Invalid date format',
+            secondary: 'Date must conform to 2014/11/25 17:30 format'
+          }
+          redirect_to(:back) and return
+        end
+      end
       @discount = update_resource(:discount, :name, :rate, :start_at, :end_at)
-      render nothing: true
+      redirect_to :back
     end
 
     private
