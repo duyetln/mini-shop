c5a0fa010c712f08543dc86bc90afcdb7dde540f
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-08-31 16:59:28 -0700

    Remove pagination helper

--- admin/app/helpers/application_helper.rb
+++ admin/app/helpers/application_helper.rb
@@ -5,17 +5,4 @@ module ApplicationHelper
       FalseClass => 'No'
     }[value.class] || value
   end
-
-  def paginate(path, prev_page, next_page, opts = {})
-    prev_li_class = prev_page.blank? ? 'disabled' : ''
-    next_li_class = next_page.blank? ? 'disabled' : ''
-
-    prev_link = prev_page.blank? ? '' : "href=#{send path, { page: prev_page }.merge(opts)}"
-    next_link = next_page.blank? ? '' : "href=#{send path, { page: next_page }.merge(opts)}"
-
-    "<ul class='pager'>
-      <li class='previous #{prev_li_class}'><a #{prev_link}><i class='fa fa-chevron-left'></i> Previous</a></li>
-      <li class='next #{next_li_class}'><a #{next_link}>Next <i class='fa fa-chevron-right'></i></a></li>
-    </ul>".html_safe
-  end
 end
