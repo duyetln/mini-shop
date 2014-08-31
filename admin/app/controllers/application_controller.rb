f7300e3384002b4b7c0f650400d4e7377b4ebc7b
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-08-31 16:58:20 -0700

    Remove clipboard controller

--- admin/app/controllers/application_controller.rb
+++ admin/app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   include ParamsHelper
   include ResourcesHelper
-  include ClipboardHelper
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
