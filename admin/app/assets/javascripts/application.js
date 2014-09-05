77ba5550e2c49caa560af631b3faaf1700b17ff6
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-09-04 22:28:46 -0700

    Add tooltip hints on small buttons, group table buttons into action column, fix small style bug

--- admin/app/assets/javascripts/application.js
+++ admin/app/assets/javascripts/application.js
@@ -2,3 +2,10 @@
 //= require bootstrap/alert
 //= require bootstrap/dropdown
 //= require bootstrap/modal
+//= require bootstrap/tooltip
+
+$(document).ready(
+  function() {
+    $('[data-toggle=tooltip]').tooltip();
+  }
+);
