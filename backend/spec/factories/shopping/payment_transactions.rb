b3a06997fb3d5267cd391116fb92c86d0be062ea
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-10-08 12:18:35 -0700

    Add factories for RefundTransaction and PaymentTransaction types

---
+++ backend/spec/factories/shopping/payment_transactions.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :payment_transaction do
+    payment_method { build :payment_method }
+    user { payment_method.user }
+    amount { 100 }
+    currency { payment_method.currency }
+    billing_address { build :address, user: user }
+  end
+end
--- backend/spec/factories/shopping/refund_transactions.rb
+++ backend/spec/factories/shopping/refund_transactions.rb
@@ -1,17 +1,9 @@
 FactoryGirl.define do
-  factory :transaction do
+  factory :refund_transaction do
     payment_method { build :payment_method }
     user { payment_method.user }
     amount { 100 }
     currency { payment_method.currency }
     billing_address { build :address, user: user }
-
-    trait :payment do
-      amount { 100 }
-    end
-
-    trait :refund do
-      amount { -100 }
-    end
   end
 end
