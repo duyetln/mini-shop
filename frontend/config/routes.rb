03e6f7593462c7482bc9dcc26437ed8fa640e371
Duyet Nguyen <duyetlnguyen@gmail.com>
2014-10-13 13:54:38 -0700

    Add basic routing config for account controller

--- frontend/config/routes.rb
+++ frontend/config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   root 'store#show'
 
   resource :store, controller: :store, only: [:show]
+  resource :account, controller: :account, only: [:show, :create] do
+    get :signin
+    get :signup
+    post :verify
+  end
 end
